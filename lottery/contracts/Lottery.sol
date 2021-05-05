0cd704bdf0cdfd6de23c552fa2a3648fe899bd67
costar <costar@costars-mbp.lan>
2021-05-05 10:13:00 -0400

    added test to assert enter function works

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -2,33 +2,36 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
-    address[] public contestants;
-    
-    function Lottery() public {
+    address[] public players;
+    address public lastWinner;
+
+    constructor() public {
         manager = msg.sender;
     }
-    
+
     function enter() public payable {
-        require(msg.value > 0.1 ether);
-        contestants.push(msg.sender);
+        require(msg.value > .0000000001 ether);
+
+        players.push(msg.sender);
     }
-    
-    function random() private view returns (uint) {
-       return uint(keccak256(block.difficulty, now, contestants));
-    } 
-    
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
+    }
+
     function pickWinner() public restricted {
-        uint index = random() % contestants.length;
-        contestants[index].transfer(this.balance); 
-        contestants = new address[](0); 
+        uint256 index = random() % players.length;
+        lastWinner = players[index];
+        players[index].transfer(this.balance);
+        players = new address[](0);
     }
-    
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function listPlayers() public view returns (address[]) {
-        return contestants;
+
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 }
--- lottery/deploy.js
+++ lottery/deploy.js
@@ -2,21 +2,25 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
+const mnemonic = 'tiny nasty there biology home gate endorse fringe skin patient nurse common';
+
 const provider = new HDWalletProvider(
-    'breeze shock pair shuffle pottery mean pair found kind scrub crane safe',
-    'https://rinkeby.infura.io/v3/830023ae9bfc4c86ac5bb1d468b5cdc8'
+     mnemonic,
+    'https://rinkeby.infura.io/v3/183938d4eb184360af056f3394ba4e43'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+    const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+    console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    const result =  await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0] });
 
-  console.log('Contract deployed to', result.options.address);
+    console.log(interface);    
+    console.log('Contract deployed', result.options.address)
 };
 deploy();
